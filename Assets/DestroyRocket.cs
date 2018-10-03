d4f93ac6b429118eb7a021fcbef0f320014b1f6c
Matthew Lucero <matthew.lucero92@yahoo.com>
2018-10-03 15:34:46 -0700

    Deleted print() statement

--- Assets/DestroyRocket.cs
+++ Assets/DestroyRocket.cs
@@ -12,8 +12,6 @@ public class DestroyRocket : MonoBehaviour {
         if (col.gameObject.name == "rocket")
         {
             Destroy(col.gameObject);
-            print("collision");
-            print(" ");
         }
     }
 }
